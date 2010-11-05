694b952e428dde76e2775b0cc948067532e4cf5a
Jeremy Kozdon <jkozdon@stanford.edu>
2010-11-05 15:43:22 -0700

    matlab files fixed.

--- execLinElast/matlab/plotData.m
+++ execLinElast/matlab/plotData.m
@@ -2,13 +2,11 @@ plot(data.x,data.tau/4,'.-','Color',C(1,:))
 hold on
 plot(data.x,data.V,'.-','Color',C(2,:))
 hold off
-% colors
-% hold on
-% for i = 1:max(data.L)
-%     I = data.L == i;
-%     plot(data.x(I),data.tau(I)/4,'.','Color',C(i,:))
-%     plot(data.x(I),data.V(I),'.','Color',C(i,:))
-% end
-% plot(data.x,data.tau/4,data.x,data.V,'LineWidth',1)
-% hold off
-
+colors
+hold on
+for i = 1:max(data.L)
+    I = data.L == i;
+    plot(data.x(I),i+0*data.tau(I)/4,'.','Color',C(i,:))
+   %  plot(data.x(I),data.V(I),'.','Color',C(i,:))
+end
+hold off
