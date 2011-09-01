74018a4476f30c2b1ec7c6b6c89f3ac4b5e8e5e8
Jeremy Edward Kozdon <jkozdon@stanford.edu>
2011-08-31 20:11:17 -0700

    extracted out some physics from IBC to reuse code

---
+++ srcLinElast/LEPhysIBC.cpp
@@ -0,0 +1,22 @@
+#include "LEPhysIBC.H"
+#include "LinElastPhysicsF_F.H"
+#include "PlasticPhysicsF_F.H"
+
+/// Set the fortran parameters for linear elasticity
+void LEPhysIBC::setFortranCommonLE(const Real& a_cs,
+    const Real& a_cp,
+    const Real& a_mu,
+    const vector<Real> a_back)
+{
+    FORT_LINELASTSETF(CHF_CONST_REAL(a_cs),CHF_CONST_REAL(a_cp),CHF_CONST_REAL(a_mu),CHF_CONST_VR(a_back));
+    m_isFortranCommonLESet = true;
+}
+
+/// Set the fortran parameters for plasticity
+void LEPhysIBC::setFortranCommonPlastic(const Real& a_mu,
+    const Real& a_beta,
+    const Real& a_eta)
+{
+    FORT_PLASTICSETF(CHF_CONST_REAL(a_mu),CHF_CONST_REAL(a_beta),CHF_CONST_REAL(a_eta));
+    m_isFortranCommonPlasticSet = true;
+}
